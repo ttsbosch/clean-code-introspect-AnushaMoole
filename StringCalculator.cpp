dcf4ee38a875d1ef58be425f2957d8dac00b85e5
AnushaMoole <anushacmoole@gmail.com>
2024-06-27 16:16:59 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -7,7 +7,10 @@
 int StringCalculator::add(string input){
         if((input == "") || (input == "0"))
         return 0;
+        return getNum(input);
+}
 
+int StringCalculator::getNum(string input){
     std::vector<int> nums;
     std::stringstream ss(input);
     std::string token;
@@ -16,25 +19,8 @@ int StringCalculator::add(string input){
     while (std::getline(ss, token, ',')) {
         nums.push_back(std::stoi(token));
     }
-
     for (int num : nums) {
         sum += num;
     }
     return sum;
 }
-
-// int StringCalculator::addNum(string input){     
-//     std::string delimiters = ",";
-//     int sum = 0;
-//     std::string numberStr = 0;
-//     size_t startPos = 0;
-//     size_t endPos = 0;
-
-//     while (endPos != std::string::npos) {
-//         endPos = input.find_first_of(delimiters, startPos);
-//         numberStr = input.substr(startPos, endPos - startPos);
-//         sum += std::stoi(numberStr);
-//         startPos = endPos + 1;
-//     }
-//     return sum;
-//}
