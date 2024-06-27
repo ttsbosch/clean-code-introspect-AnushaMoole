7609be30b2bf3e6977ba5b995440ff6ef238beef
AnushaMoole <anushacmoole@gmail.com>
2024-06-27 15:27:07 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -3,4 +3,21 @@
 int StringCalculator::add(string input){
         if((input == "") || (input == "0"))
         return 0;
+        else
+                addNum(input);
+}
+
+int StringCalculator::add(string input){
+    std::string delimiters = ",;\n*\t/";
+    std::string token;
+    size_t pos = 0;
+    int sum = 0;
+    int num = 0;
+    while ((pos = input.find_first_of(delimiters, pos)) != std::string::npos) {
+        token = input.substr(0, pos);
+        num = str::stoi(token);
+        input.erase(0, pos + 1); // Remove the delimiter and the extracted token
+        pos = 0; // Reset the position for the next search
+        sum += num;
+    }
 }
