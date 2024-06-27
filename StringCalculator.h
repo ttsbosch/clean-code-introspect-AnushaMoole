b5533b691b59ebef43d8bd4a4d8cb4c1134c200d
AnushaMoole <anushacmoole@gmail.com>
2024-06-27 17:02:46 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -8,5 +8,6 @@ class StringCalculator{
     public:
     int add(string input);
     std::vector<int> getNum(string input);
-    std::vector<int> removeNegative(std::vector<int> nums);
+    //std::vector<int> removeNegative(std::vector<int> nums);
+    int accumulate(std::vector<int> vectors);
 };
