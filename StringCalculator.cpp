5beb309721d867397dddf54927f2bbfb2bc514fc
AnushaMoole <anushacmoole@gmail.com>
2024-06-27 15:57:41 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,26 +1,40 @@
 #include "StringCalculator.h"
 #include "sstream"
 #include "string"
+#include "vector"
 
 int StringCalculator::add(string input){
         if((input == "") || (input == "0"))
         return 0;
-        else
-        return addNum(input);
+
+    std::vector<int> nums;
+    std::stringstream ss(numbers);
+    std::string token;
+
+    while (std::getline(ss, token, ',')) {
+        nums.push_back(std::stoi(token));
+    }
+
+    int sum = 0;
+    for (int num : nums) {
+        sum += num;
+    }
+
+    return sum;
 }
 
-int StringCalculator::addNum(string input){     
-    std::string delimiters = ",;\n*\t/";
-    int sum = 0;
-    std::string numberStr = 0;
-    size_t startPos = 0;
-    size_t endPos = 0;
+// int StringCalculator::addNum(string input){     
+//     std::string delimiters = ",";
+//     int sum = 0;
+//     std::string numberStr = 0;
+//     size_t startPos = 0;
+//     size_t endPos = 0;
 
-    while (endPos != std::string::npos) {
-        endPos = input.find_first_of(delimiters, startPos);
-        numberStr = input.substr(startPos, endPos - startPos);
-        sum += std::stoi(numberStr);
-        startPos = endPos + 1;
-    }
-    return sum;
+//     while (endPos != std::string::npos) {
+//         endPos = input.find_first_of(delimiters, startPos);
+//         numberStr = input.substr(startPos, endPos - startPos);
+//         sum += std::stoi(numberStr);
+//         startPos = endPos + 1;
+//     }
+//     return sum;
 }
