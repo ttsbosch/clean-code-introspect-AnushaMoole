499d7d7ea8719e613fd343c4c9b4e2c336f2ba4a
AnushaMoole <anushacmoole@gmail.com>
2024-06-27 15:41:14 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,4 +1,5 @@
 #include "StringCalculator.h"
+#include "sstream"
 #include "string"
 
 int StringCalculator::add(string input){
@@ -8,18 +9,13 @@ int StringCalculator::add(string input){
                 addNum(input);
 }
 
-int StringCalculator::addNum(string input){
-    std::string delimiters = ",;\n*\t/";
+int StringCalculator::addNum(string input){     
+    std::stringstream ss(input);
     std::string token;
-    size_t pos = 0;
     int sum = 0;
-    int num = 0;
-        
-    while ((pos = input.find_first_of(delimiters, pos)) != std::string::npos) {
-        token = input.substr(0, pos);
-        num = std::stoi(token);
-        sum += num;
-        input.erase(0, pos + 1); // Remove the delimiter and the extracted token
-        pos = 0; // Reset the position for the next search
+
+    while (std::getline(ss, token, ',;\n*\t/')) {
+        sum += std::stoi(token);
     }
+    return sum;
 }
