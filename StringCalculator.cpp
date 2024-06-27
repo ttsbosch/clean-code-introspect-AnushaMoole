6057773f3439ad16b8d1593dd5a5bb9ef0454520
AnushaMoole <anushacmoole@gmail.com>
2024-06-27 15:36:14 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -14,11 +14,12 @@ int StringCalculator::addNum(string input){
     size_t pos = 0;
     int sum = 0;
     int num = 0;
+        
     while ((pos = input.find_first_of(delimiters, pos)) != std::string::npos) {
         token = input.substr(0, pos);
         num = std::stoi(token);
+        sum += num;
         input.erase(0, pos + 1); // Remove the delimiter and the extracted token
         pos = 0; // Reset the position for the next search
-        sum += num;
     }
 }
